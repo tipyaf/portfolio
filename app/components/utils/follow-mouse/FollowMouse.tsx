441de675a9dc16ac84607a72ceb510486c27b217
yannickb <ya.net@hotmail.fr>
2024-08-10 04:22:21 +0200

    feat(config): prettier + IDE config

--- app/components/utils/follow-mouse/FollowMouse.tsx
+++ app/components/utils/follow-mouse/FollowMouse.tsx
@@ -1,49 +1,52 @@
-"use client";
+'use client';
 
-import {motion} from "framer-motion";
-import {useEffect, useState} from "react";
-import {useMovingMouse} from "@/app/hooks/useMovingMouse";
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useMovingMouse } from '@/app/hooks/useMovingMouse';
 
 interface ImageCursorFollowerProps {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
 // Linear interpolation function
 const lerp = (start: number, end: number, factor: number) => start + (end - start) * factor;
 
-export default function FollowMouse({children, className = ''}: Readonly<ImageCursorFollowerProps>) {
-    const {mainRef, currentX, currentY, targetX, targetY, setCurrentX, setCurrentY} = useMovingMouse();
+export default function FollowMouse({
+  children,
+  className = '',
+}: Readonly<ImageCursorFollowerProps>) {
+  const { mainRef, currentX, currentY, targetX, targetY, setCurrentX, setCurrentY } =
+    useMovingMouse();
 
-    const [isInitialAnimationDone] = useState(false);
-    // Handle animation based on mouse movement after the initial animation
-    useEffect(() => {
-        if (isInitialAnimationDone) {
-            let animationFrameId: number;
+  const [isInitialAnimationDone] = useState(false);
+  // Handle animation based on mouse movement after the initial animation
+  useEffect(() => {
+    if (isInitialAnimationDone) {
+      let animationFrameId: number;
 
-            const animate = () => {
-                setCurrentX(prevX => lerp(prevX, targetX, 0.01));
-                setCurrentY(prevY => lerp(prevY, targetY, 0.02));
+      const animate = () => {
+        setCurrentX((prevX) => lerp(prevX, targetX, 0.01));
+        setCurrentY((prevY) => lerp(prevY, targetY, 0.02));
 
-                animationFrameId = requestAnimationFrame(animate);
-            };
-            animate();
+        animationFrameId = requestAnimationFrame(animate);
+      };
+      animate();
 
+      return () => {
+        cancelAnimationFrame(animationFrameId);
+      };
+    }
+  }, [isInitialAnimationDone, targetX, targetY, setCurrentX, setCurrentY]);
 
-            return () => {
-                cancelAnimationFrame(animationFrameId);
-            };
-        }
-    }, [isInitialAnimationDone, targetX, targetY, setCurrentX, setCurrentY]);
-
-    return (
-        <motion.div
-            ref={mainRef}
-            className={`${className} cursor-grab`}
-            transition={{ease: "easeInOut", duration: 0.75}}
-            style={{translateX: `${currentX}%`, translateY: `${-currentY}%`}}
-        >
-            {children}
-        </motion.div>
-    );
+  return (
+    <motion.div
+      ref={mainRef}
+      className={`${className} cursor-grab`}
+      transition={{ ease: 'easeInOut', duration: 0.75 }}
+      style={{ translateX: `${currentX}%`, translateY: `${-currentY}%` }}
+    >
+      {children}
+    </motion.div>
+  );
 }
--- app/hooks/useMovingMouse.ts
+++ app/hooks/useMovingMouse.ts
@@ -4,51 +4,53 @@ import { useEffect, useState, useRef } from 'react';
 const lerp = (start: number, end: number, factor: number) => start + (end - start) * factor;
 
 export function useMovingMouse() {
-    const [currentX, setCurrentX] = useState(50);
-    const [currentY, setCurrentY] = useState(50);
-    const [targetX, setTargetX] = useState(50);
-    const [targetY, setTargetY] = useState(50);
+  const [currentX, setCurrentX] = useState(50);
+  const [currentY, setCurrentY] = useState(50);
+  const [targetX, setTargetX] = useState(50);
+  const [targetY, setTargetY] = useState(50);
 
-    const mainRef = useRef<HTMLDivElement>(null);
+  const mainRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const handleMouseMove = (e: MouseEvent) => {
-            if (mainRef.current) {
-                const containerRect = mainRef.current.getBoundingClientRect();
-                const containerWidth = containerRect.width;
-                const containerHeight = containerRect.height;
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (mainRef.current) {
+        const containerRect = mainRef.current.getBoundingClientRect();
+        const containerWidth = containerRect.width;
+        const containerHeight = containerRect.height;
 
-                const mousePositionInContainerX = ((e.clientX - containerRect.left / 2) / containerWidth) * 100;
-                const mousePositionInContainerY = ((e.clientY - containerRect.top / 2) / containerHeight) * 100;
+        const mousePositionInContainerX =
+          ((e.clientX - containerRect.left / 2) / containerWidth) * 100;
+        const mousePositionInContainerY =
+          ((e.clientY - containerRect.top / 2) / containerHeight) * 100;
 
-                setTargetX(mousePositionInContainerX);
-                setTargetY(100 - mousePositionInContainerY); // Invert Y for the background
-            }
-        };
+        setTargetX(mousePositionInContainerX);
+        setTargetY(100 - mousePositionInContainerY); // Invert Y for the background
+      }
+    };
 
-        window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener('mousemove', handleMouseMove);
 
-        return () => {
-            window.removeEventListener("mousemove", handleMouseMove);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
-    useEffect(() => {
-        let animationFrameId: number;
+  useEffect(() => {
+    let animationFrameId: number;
 
-        const animate = () => {
-            setCurrentX(prevX => lerp(prevX, targetX, 0.05));
-            setCurrentY(prevY => lerp(prevY, targetY, 0.05));
+    const animate = () => {
+      setCurrentX((prevX) => lerp(prevX, targetX, 0.05));
+      setCurrentY((prevY) => lerp(prevY, targetY, 0.05));
 
-            animationFrameId = requestAnimationFrame(animate);
-        };
+      animationFrameId = requestAnimationFrame(animate);
+    };
 
-        animate();
+    animate();
 
-        return () => {
-            cancelAnimationFrame(animationFrameId);
-        };
-    }, [targetX, targetY]);
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, [targetX, targetY]);
 
-    return { mainRef, currentX, currentY, targetX, targetY, setCurrentX, setCurrentY };
+  return { mainRef, currentX, currentY, targetX, targetY, setCurrentX, setCurrentY };
 }
