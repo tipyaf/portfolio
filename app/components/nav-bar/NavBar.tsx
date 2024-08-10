743b22c0fd0d10a086f1928fca8a509a78d1b1f7
yannickb <ya.net@hotmail.fr>
2024-08-10 04:22:21 +0200

    feat(config): prettier + IDE config

--- app/components/nav-bar/NavBar.tsx
+++ app/components/nav-bar/NavBar.tsx
@@ -1,23 +1,22 @@
-"use client";
+'use client';
 
-import {useEffect, useState} from "react";
-import css from "./NavBar.module.css"
-import {FiDownloadCloud} from "react-icons/fi";
-import Button from "@/app/components/utils/Button";
+import { useEffect, useState } from 'react';
+import css from './NavBar.module.css';
+import { FiDownloadCloud } from 'react-icons/fi';
+import Button from '@/app/components/utils/Button';
 
 export default function NavBar() {
-
-    const [scroll, setScroll] = useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            setScroll(window.scrollY >= 90);
-        });
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScroll(window.scrollY >= 90);
     });
-    return (
-        <header className={`${css.navBar} ${scroll ? css.scrolled : css.initial} p-3`}>
-            <nav className="flex align-center justify-end">
-                <Button icon={FiDownloadCloud}>Check out my resume</Button>
-            </nav>
-        </header>
-    )
+  });
+  return (
+    <header className={`${css.navBar} ${scroll ? css.scrolled : css.initial} p-3`}>
+      <nav className="align-center flex justify-end">
+        <Button icon={FiDownloadCloud}>Check out my resume</Button>
+      </nav>
+    </header>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-"use client"
+'use client';
 
-import NavBar from "@/app/components/nav-bar/NavBar";
-import HomeSection from "@/app/components/home-section/HomeSection";
+import NavBar from '@/app/components/nav-bar/NavBar';
+import HomeSection from '@/app/components/home-section/HomeSection';
 
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between">
-            <NavBar/>
-            <HomeSection/>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <NavBar />
+      <HomeSection />
+    </main>
+  );
 }
