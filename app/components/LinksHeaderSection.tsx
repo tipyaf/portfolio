9f7285fde3fe13e09d0a9eae0d6de99608f94d99
yannickb <ya.net@hotmail.fr>
2024-08-13 02:05:22 +0200

    fix(AboutPage): fix stacktrace from id of list iteration + adjust responsive display

--- app/components/LinksHeaderSection.tsx
+++ app/components/LinksHeaderSection.tsx
@@ -24,20 +24,21 @@ const socialLinks: ILink[] = [
 
 export function LinksHeaderSection() {
   return (
-    <ul className="absolute right-3 top-3 flex gap-4 font-bold text-current">
+    <ul className="absolute left-0 top-3 flex w-screen justify-center font-bold text-current sm:justify-end">
       {socialLinks.map((link, i) => (
-        <>
-          <li key={i} className="border-l border-current px-4">
-            <Button
-              className="flex flex-row-reverse items-center gap-1 hover:underline"
-              href={link.href}
-              target="_blank"
-              icon={link.icon}
-            >
-              {link.label}
-            </Button>
-          </li>
-        </>
+        <li
+          key={i}
+          className="border-l-2 border-solid border-current px-4 last:border-r-2 last:sm:border-r-0"
+        >
+          <Button
+            className="flex flex-row-reverse items-center gap-1 hover:underline"
+            href={link.href}
+            target="_blank"
+            icon={link.icon}
+          >
+            {link.label}
+          </Button>
+        </li>
       ))}
     </ul>
   );
